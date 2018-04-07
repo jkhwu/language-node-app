1a04e1a64b47e520fde6c7930e13d47833e2b872
Janet Hwu <Janet@MacBook-Pro-2.local>
2018-04-06 23:10:46 -0700

    finished

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # language-node-app
 an exercise using node to request data from APIs
+
+### Overview
+
+This is a simple node app that takes commands via the terminal and returns data from the Twitter, Spotify, and OMDB APIs.
+
+### Links
+
+* [Twitter](https://www.npmjs.com/package/twitter)
+
+* [Node-Spotify-API](https://www.npmjs.com/package/node-spotify-api)
+
+* [OMDB API](http://www.omdbapi.com).
+
+
+### Authentication
+
+Note that you will have to create a local file named `.env` and enter your own Twitter and Spotify keys as follows:
+
+```
+# Spotify API keys
+
+SPOTIFY_ID=your-spotify-id
+SPOTIFY_SECRET=your-spotify-secret
+
+# Twitter API keys
+
+TWITTER_CONSUMER_KEY=your-twitter-consumer-key
+TWITTER_CONSUMER_SECRET=your-twitter-consumer-secret
+TWITTER_ACCESS_TOKEN_KEY=your-access-token-key
+TWITTER_ACCESS_TOKEN_SECRET=your-twitter-access-token-secret
+```
+* Get your Twitter API keys (4) at <https://apps.twitter.com/app/new>
+* Get your Spotify API keys (2) by going to <https://developer.spotify.com/my-applications/#!/> and creating a new app.
+* Copy and paste the 6 keys into your local `.env` file.
+
+### Setup
+Note all dependencies in the package.json file and install them before running the app.
+
+### Usage
+
+Run the file by command line by navigating to the project directory and entering `node liri.js` followed immediately by one of four commands:
+
+* `my-tweets` (gets my latest 20 tweets)
+* `spotify-this-song` followed by a song name of your choosing in quotation marks, e.g. `"Salty Eyes` (logs song data to console)
+* `movie-this` followed by a movie title of your choosing in quotation marks, e.g. `"Toy Story"` (logs movie data to console)
+* `do-what-it-says` (reads the file content of `random.txt` and executes the command therein)
+
+Note: This app will default to searching its own titles if user does not enter one.
+
+### Access
+
+This app can also be accessed from my [portfolio page](https://jkhwu.github.io/Responsive-Portfolio/portfolio.html?vs=1) as a demo of node practice.
+
+
+
--- liri.js
+++ liri.js
@@ -1,85 +1,90 @@
-require("dotenv").config();
+// Requires
+require('dotenv').config();
 const keys = require('./keys.js')
 const Twitter = require('twitter');
 const Spotify = require('node-spotify-api');
 const request = require('request');
 const fs = require("fs")
 
+// Global Variables
 let spotify = new Spotify(keys.spotify);
 let client = new Twitter(keys.twitter);
 let [node, path, command, title] = process.argv;
 
+// Logs input to console, appends input to log.txt file
+let addLog = (logx) => {
+    console.log(logx);
+    fs.appendFile('./log.txt', (`${logx}\n`), (err) => {
+        if (err) throw err;
+    });
+}
+
+// Gets 20 latest tweets from specified Twitter user
 let myTweets = () => {
-    console.log('These are your last 20 tweets:');
+    addLog('These are your last 20 tweets:');
     var params = { screen_name: 'jkhwu1' };
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
-            for (let i = 0; i < Math.min(tweets.length, 20); i++) console.log(tweets[i].created_at + ": " + tweets[i].text);
-        } else console.log(error);
+            let logThis = "";
+            for (let i = 0; i < Math.min(tweets.length, 20); i++) {
+                logThis += `${tweets[i].text}\n(${tweets[i].created_at})\n`;
+            }
+            addLog(logThis);
+        } else addLog(error);
     });
 }
 
-let spotifyThis = () => {
-    console.log('OK, looking up the song "' + title.trim() + '" ...');
-    spotify.search({ type: 'track', query: title.trim() }, function(err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
+// Looks up song on Spotify, logs certain data
+let spotifyThis = (songx) => {
+    addLog('OK, looking up the song ' + songx + ' ...');
+    spotify.search({ type: 'track', query: songx }, function(err, data) {
+        if (err) addLog('Error occurred: ' + err);
         let result = data.tracks.items[0];
-        // console.log(JSON.stringify(result, null, 2));
-        console.log("SONG: " + result.name);
-        console.log("ARTIST(S):");
+        let logThis = `SONG: ${result.name} \nLINK: ${result.external_urls.spotify} \nALBUM: ${result.album.name} \nARTIST(S):\n`;
         result.artists.forEach(artist => {
-            console.log(artist.name);
+            logThis += `${artist.name}\n`;
         });
-        console.log("LINK: " + result.external_urls.spotify);
-        console.log("ALBUM: " + result.album.name);
+        addLog(logThis);
     });
 }
 
-let movieThis = () => {
-    console.log('OK, looking up the movie "' + title.trim() + '" ...');
-    let url = "http://www.omdbapi.com/?t=" + title.trim() + "&y=&plot=short&apikey=trilogy&r=json";
+// Looks up Movie on OMDB, logs certain data
+let movieThis = (namex) => {
+    addLog('OK, looking up the movie ' + namex + ' ...');
+    let url = "http://www.omdbapi.com/?t=" + namex + "&y=&plot=short&apikey=trilogy&r=json";
     request(url, function(error, response, body) {
-        if (error) return console.log('error:', error);
-        // console.log('statusCode:', response && response.statusCode);
-        // console.log('body:', JSON.parse(body))
+        if (error) return addLog('error:', error);
         let result = JSON.parse(body)
-        console.log("TITLE: " + result.Title);
-        console.log("YEAR: " + result.Year);
-        console.log("IMDB RATING: " + result.Ratings[0].Value);
-        console.log("ROTTEN TOMATOES RATING: " + result.Ratings[1].Value);
-        console.log("COUNTRY: " + result.Country);
-        console.log("LANGUAGE: " + result.Language);
-        console.log("PLOT: " + result.Plot);
-        console.log("ACTORS: " + result.Actors);
-    });
-
-}
-
-let doIt = () => {
-    console.log('Reading command from random.txt ...');
-    fs.readFile('./random.txt', 'utf8', (err, data) => {
-        if (err) throw err;
-        console.log(data)
+        let logThis = `TITLE: ${result.Title}\nYEAR: ${result.Year}\nIMDB RATING: ${result.Ratings[0].Value}\nROTTEN TOMATOES RATING: ${result.Ratings[1].Value}\nCOUNTRY: ${result.Country}\nLANGUAGE: ${result.Language}\nPLOT: ${result.Plot}\nACTORS: ${result.Actors}\n`
+        addLog(logThis);
     });
 }
 
-switch (command) {
-    case 'my-tweets':
-        myTweets();
-        break;
-    case 'spotify-this-song':
-        if (!title) title = "'The Sign' Ace of Base";
-        spotifyThis();
-        break;
-    case 'movie-this':
-        if (!title) title = "Mr. Nobody";
-        movieThis();
-        break;
-    case 'do-what-it-says':
-        doIt();
-        break;
-    default:
-        console.log('Sorry, cannot execute ' + command + '.');
+// Reads random.txt file for 'command' and 'title' parameters
+let doIt = () => fs.readFileSync('./random.txt', 'utf8').split(',');
+
+// Handles user commands from terminal
+let runSwitch = (commandx, titlex) => {
+    switch (commandx) {
+        case 'my-tweets':
+            myTweets();
+            break;
+        case 'spotify-this-song':
+            if (!titlex) titlex = "'The Sign' Ace of Base";
+            spotifyThis(titlex.trim());
+            break;
+        case 'movie-this':
+            if (!titlex) titlex = "Mr. Nobody";
+            movieThis(titlex.trim());
+            break;
+        case 'do-what-it-says':
+            console.log('Reading command from random.txt ...');
+            runSwitch(doIt()[0], doIt()[1]);
+            break;
+        default:
+            console.log('Sorry, cannot execute ' + commandx + '.');
+    }
 }
+
+addLog(`${command.trim()}:`)
+runSwitch(command.trim(), title);
