f2a56bdd7a90541e4830ed4591d36911b5f84d5f
Janet Hwu <Janet@MacBook-Pro-2.local>
2018-04-06 18:43:02 -0700

    wrote myTweets function

--- liri.js
+++ liri.js
@@ -2,23 +2,46 @@ require("dotenv").config();
 let keys = require('./keys.js')
 let Twitter = require('twitter');
 let Spotify = require('node-spotify-api');
+// let request = require('request');
 let spotify = new Spotify(keys.spotify);
 let client = new Twitter(keys.twitter);
-
 let [node, path, command, title] = process.argv;
+
+let myTweets = () => {
+    console.log('These are your last 20 tweets:');
+    var params = { screen_name: 'jkhwu1' };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (let i = 0; i < Math.min(tweets.length, 20); i++) console.log(tweets[i].created_at + ": " + tweets[i].text);
+        } else console.log(error);
+    });
+}
+
+let spotifyThis = () => {
+    console.log('OK, looking up your song: ' + title);
+
+}
+
+let movieThis = () => {
+    console.log('OK, looking up your movie: ' + title);
+}
+
+let doIt = () => {
+    console.log('Reading command from random.txt ...');
+}
 
 switch (command) {
     case 'my-tweets':
-        console.log('These are your last 20 tweets:');
+        myTweets();
         break;
     case 'spotify-this-song':
-        console.log('OK, looking up your song: ' + title);
+        spotifyThis();
         break;
     case 'movie-this':
-        console.log('OK, looking up your movie: ' + title);
+        movieThis();
         break;
     case 'do-what-it-says':
-        console.log('Reading command from random.txt ...');
+        doIt();
         break;
     default:
         console.log('Sorry, cannot execute ' + command + '.');
